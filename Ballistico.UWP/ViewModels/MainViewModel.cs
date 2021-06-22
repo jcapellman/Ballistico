82d71be911fce5641f81ef906ed2f914a56d105d
Jarred Capellman <jcapellman@hotmail.com>
2021-06-22 18:23:34 -0500

    Added: Initial commit

---
+++ Ballistico.UWP/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Ballistico.UWP.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
