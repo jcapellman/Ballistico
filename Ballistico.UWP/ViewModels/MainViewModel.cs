ab11f042c8a2e7e0802e478c17e374c6ce8811cd
Jarred Capellman <jcapellman@hotmail.com>
2021-06-26 16:43:45 -0500

    Added: WinUI and continued work on the UI/UX

--- Ballistico.UWP/ViewModels/MainViewModel.cs
+++ Ballistico.UWP/ViewModels/MainViewModel.cs
@@ -4,6 +4,20 @@ namespace Ballistico.UWP.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        private int weight;
+
+        public int Weight
+        {
+            get => weight;
+
+            set
+            {
+                weight = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Weight)));
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }
